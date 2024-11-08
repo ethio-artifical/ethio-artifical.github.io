1738c5213473330678c34eb96d13d1f99827495c
Anteneh Yehalem <99423830+ethio-artifical@users.noreply.github.com>
2024-11-08 23:33:20 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function showLinks() {
+  const linksContainer = document.getElementById("linksContainer");
+  linksContainer.style.display = linksContainer.style.display === "none" ? "block" : "none";
+}
